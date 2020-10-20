7831f74a168c5294ec96d90c0c0a343736d4dab1
Victor Hernandez <vik407@gmail.com>
2020-10-19 21:54:05 -0500

    pep8 whitespace after:

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list:List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """ Returns list of floats/int
     """
     return sum(mxd_list)
