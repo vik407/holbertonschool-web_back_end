1fc37a27ff2af79ed74537a830048e74f8559d05
Victor Hernandez <vik407@gmail.com>
2020-10-19 21:46:49 -0500

    Basic 2 string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """ Returns string representation of float number
+    """
+    return str(n)
