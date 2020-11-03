3643c8f3e8ffb7cd99e8614f1fb552620b4a7bdd
Victor Hernandez <vik407@gmail.com>
2020-11-02 22:30:34 -0500

    Encrypt and test passwords using bcrypt

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     """ Salted pass generation
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is valid?
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
