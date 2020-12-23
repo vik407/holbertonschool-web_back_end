7b7fda92f6ba204770690b19a418b0b286994d62
Victor Hernandez <vik407@gmail.com>
2020-12-23 11:00:23 -0500

    More singles

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
   .then(() => ({
     status: 200,
-    body: "success",
+    body: 'success',
   }))
   .catch(() => new Error())
   .finally(() => {
-    console.log("Got a response from the API");
+    console.log('Got a response from the API');
   });
 }
--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return uploadPhoto()
@@ -7,7 +7,7 @@ export default function handleProfileSignup() {
         .then(({ firstName, lastName }) => {
           console.log(`${body} ${firstName} ${lastName}`);
         })
-        .catch(() => console.log("Signup system offline"));
+        .catch(() => console.log('Signup system offline'));
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
