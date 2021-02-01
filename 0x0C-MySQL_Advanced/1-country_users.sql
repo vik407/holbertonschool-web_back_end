154eef19a6961ea0c2c309a5bac0ca05a5bd1c9e
Victor Hernandez <vik407@gmail.com>
2021-01-31 23:05:09 -0500

    Task 1-3

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a users table
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL
+);
---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all bands with Glam as their main style, ranked by their longevity
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%';
